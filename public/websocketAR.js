27ddab3418b02d48d4e17a856166b80a9d528291
HooKim <hooo.dev@gmail.com>
2023-07-05 17:54:39 +0900

    separate emitter and listener on the both client and server side

--- public/websocketAR.js
+++ public/websocketAR.js
@@ -2,9 +2,25 @@ const viewer = document.querySelector('westmoon-viewer')
 
 const socket = io()
 
+const alertMsg = document.getElementById('alert-message')
+const coord = document.getElementById('coord')
+
 const rangeX = document.getElementById('rangeX')
 const rangeY = document.getElementById('rangeY')
 const rangeZ = document.getElementById('rangeZ')
+
+// 0 is prev 1 is current
+const isAvailable = [true, true]
+const checkAvail = setInterval(() => {
+    console.log(isAvailable)
+    if(!isAvailable[1]){
+        socket.emit('waiting', null);
+    }
+    if(isAvailable[1] && !isAvailable[0]){
+        window.alert('back to line!')
+        socket.emit('join-the-channel', null)
+    }
+}, 1000)
 
 // control from the other client!
 socket.on('remote-ar-control', (data)=>{
@@ -21,13 +37,29 @@ socket.on('remote-ar-control', (data)=>{
 // alert message when 2 clients are already using the control channel
 socket.on('full-alert', () => {
     window.alert('Line is busy : connection failed!')
-    socket.emit('waiting', null)
+
+    alertMsg.textContent = 'Please wait...'
+    coord.style.display = "none"
+
+    isAvailable[0] = isAvailable[1]
+    isAvailable[1] = false
 })
 
 // client is waitng for the empty seat
 socket.on('waiting', (msg) => {
-    if(msg) socket.emit('join-the-channel', null)
-    else socket.emit('waiting', null)
+    if(msg){
+        console.log('waiting DONE')
+        window.location.href = '/'
+        isAvailable[0] = isAvailable[1]
+        isAvailable[1] = true
+        coord.style.display = 'block'
+        alertMsg.textContent = ''
+    }else{
+        console.log('waiting????')
+        isAvailable[0] = isAvailable[1]
+        isAvailable[1] = false 
+    } 
+    
 })
 
 const slideHandler = () => {     
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const path = require('path')
-const HTTP = require('http')
-const ws = require('socket.io')
-const queue = require('queue')
+const http = require('http')
+const { Server } = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
 const PORT = process.env.PORT || 8080
 
-const app = express()
-const http = HTTP.Server(app)
-const socketIO = ws(http)
-
+// channel 
 const users = {}
 
-// waiting queue
+// waiting queue (later will be in class form)
 const MAX_CAPACITY = 1000
 const waitingQueue = new Array(MAX_CAPACITY)
 let curWaitings = 0;
@@ -27,64 +27,73 @@ const idxOp = (type, num) => {
 
 app.use(express.static('public')) // the name of static folder : public
 
+io.on('connection', (socket) => {  
 
-socketIO.on('connection', (socket) => {              
-    console.log(`${socket.id} user just connected`) 
+    console.log(`${socket.id} user just connected`)
+
+    // check the number of users in the channel 
     const curUsers = Object.keys(users)
     let curSocketIdx = -1
-    // allow only two users at the same time 
-    if(curUsers.length >= 2){
+    // if two users are already in the channel 
+    if(curUsers.length >= 2 && !(socket.id in curUsers)){
         socket.emit('full-alert', null)
     }else{
         users[socket.id] = socket
-        
-        socket.on('remote-ar-control', (data) => { // when socket recieves a message
-            socket.broadcast.emit('remote-ar-control', data)
-        })
+    }
+    // repeater
+    const checkAvail = setInterval(() => {
+        const howManyNow = Object.keys(users).length
+        console.log(curSocketIdx, frontOfLineIdx)
+        if(howManyNow < 2 && (curSocketIdx === frontOfLineIdx)){
+            socket.emit('waiting', true)
+        }
+        if(howManyNow >= 2 && curSocketIdx !== -1){
+            socket.emit('waiting', false)
+        }
+    }, 1000)
 
-        socket.on('waiting', () => {
-            if(curSocketIdx < 0){
-                //if queue is full
-                if(curWaitings + 1 === MAX_CAPACITY){
-                    socket.emit('terminate', null)
-                }
+    socket.on('remote-ar-control', (data) => { // when socket recieves a message
+        socket.broadcast.emit('remote-ar-control', data)
+    })
 
+    socket.on('waiting', () => {
+        if(curSocketIdx < 0){
+            //if queue is full
+            if(curWaitings + 1 === MAX_CAPACITY){
+                socket.emit('terminate', null)
+            }
+            else{
+                curWaitings += 1
                 // where to put to socket id
-                endOfLineIdx = idxOp('+', endOfLineIdx)
                 
                 curSocketIdx = endOfLineIdx
+                endOfLineIdx = idxOp('+', endOfLineIdx)
                 waitingQueue[curSocketIdx] = socket.id
             }
+        }
+    })
 
-            // only send message to the oldest socket
-            if(curSocketIdx === frontOfLineIdx){
-                const howManyNow = Object.keys(users).length
-                if(howManyNow < 2) {
-                    socket.emit('waiting', true)
-                    clearInterval(interval)
-                }
-                else socket.emit('waiting', false)
-            }
-        })
+    socket.on('join-the-channel', () => {
+        waitingQueue[curSocketIdx] = null
+        curSocketIdx = -1
+        if(--curWaitings) frontOfLineIdx = idxOp('+', frontOfLineIdx)
+        else frontOfLineIdx = 0
+    })
 
-        socket.on('join-the-channel', () => {
-            waitingQueue[curSocketIdx] = null
-            curSocketIdx = -1
-            frontOfLineIdx = idxOp('+', frontOfLineIdx)
-            curWaitings--;
-        })
-    
-        socket.on('disconnect', () => {
-            delete users[socket.id]
-            console.log('A user disconnected!')
-        })
-    }
+    socket.on('disconnect', () => {
+        delete users[socket.id]
+        console.log(`${socket.id} disconnected!`)
+    })
 })
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-http.listen(PORT, () => {
+app.get('/waiting', (req, res) => {
+    res.sendFile(path.join(__dirname, 'waiting.html'))
+})
+
+server.listen(PORT, () => {
     console.log(`App listening on ${PORT}`)
 })
